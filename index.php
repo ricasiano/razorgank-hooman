4f64b31e18e17753023df1b32a9277df1d475e52
rai <ricasiano@gmail.com>
2014-11-30 22:28:12 +0800

    nginx nga pala, di apache

--- index.php
+++ index.php
@@ -161,7 +161,7 @@
                     <img src="img/shell.png" />
                 </div>
                 <div class="col-lg-4">
-                    <p>What would you spend for <b><i class="context">20 pesos</i></b>? Have your website hosted here! For <b><i class="context">P20.00 a month ( $0.44 ) </i></b>, you'll have your own web space running on LAMP stack. Suitable for personal websites, small business sites, prototyping or you just want to impress your prof for your thesis!</p>
+                    <p>What would you spend for <b><i class="context">20 pesos</i></b>? Have your website hosted here! For <b><i class="context">P20.00 a month ( $0.44 ) </i></b>, you'll have your own web space running on LNMP stack. Suitable for personal websites, small business sites, prototyping or you just want to impress your prof for your thesis!</p>
 		    <br /><br />
                     <a href="#hostingInfo" class="btn btn-lg btn-outline" data-toggle="modal">
                         <i class="fa fa-globe"></i> More Info...
